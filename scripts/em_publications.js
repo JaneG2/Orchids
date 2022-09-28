b354562aa24e6241dd6920851b75a3549ac72677
JaneJG <69372062+JaneG2@users.noreply.github.com>
2022-09-29 09:04:35 +1300

    last bunch of folders

--- scripts/em_publications.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-
-user = "gaeldonaghy";
-site = "gmail.com";
-subject = "?subject=Native Orchid Group Publications"
-document.write('<a href=\"mailto:' + user + '@' + site + subject + '\">');
-document.write(user + '@' + site + '</a>');
