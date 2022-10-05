5c1a0c0a14b695730ee86137d5bb7131efcd396e
JaneJG <69372062+JaneG2@users.noreply.github.com>
2022-10-06 08:32:10 +1300

    major updates

---
+++ Scripts/em_MP.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+
+user = "michael";
+site = "nativeorchids.co.nz";
+subject = "?subject=NZ Native Orchids"
+document.write('<a href=\"mailto:' + user + '@' + site + subject + '\">');
+document.write(user + '@' + site + '</a>');
