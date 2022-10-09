357551e29eb1ab006ed814f4406825053c593d9f
JaneJG <69372062+JaneG2@users.noreply.github.com>
2022-10-10 09:55:32 +1300

    Added graphics and removed
    added grpghics folder and removed contents of assets

--- _assets/em_Treas.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-
-user = "pam";
-site = "insidetrack.co.nz";
-subject = "?subject=NZ Native Orchid Group"
-document.write('<a href=\"mailto:' + user + '@' + site + subject + '\">');
-document.write(user + '@' + site + '</a>');
